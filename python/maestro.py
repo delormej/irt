063d5d3e612901b2f8ef17393d6ad6f457f92a98
delormej <delormej@hotmail.com>
2013-10-17 22:02:53 -0400

    Added getPostion() method

--- python/maestro.py
+++ python/maestro.py
@@ -4,9 +4,11 @@
 # By Jason De Lorme <jjdelorme@yahoo.com>
 # 
 
-import serial, time
+import serial, time, struct
 
 SET_TARGET_COMMAND=0x84
+GET_POSITION_COMMAND=0x90
+
 RESISTANCE_0=8428
 RESISTANCE_1=5090
 RESISTANCE_2=4600
@@ -27,6 +29,20 @@ class Maestro(object):
 	def close(self):
 		self.s.close()
 
+	def getPosition(self):
+		self.s.write( \
+			chr(GET_POSITION_COMMAND) + \
+			chr(self.channel))
+
+		r = self.s.read(2)
+		if r is not None:
+			pos = struct.unpack('h', r)
+		else:
+			return None
+		
+		return pos[0]
+
+
 	def setTarget(self, position):
 		low = position&0x7f
 		high = position>>7
@@ -38,16 +54,16 @@ class Maestro(object):
 			chr(high))
 
 	def setResistance0(self):
-		setTarget(RESISTANCE_0)
+		self.setTarget(RESISTANCE_0)
 
 	def setResistance1(self):
-		setTarget(RESISTANCE_1)
+		self.setTarget(RESISTANCE_1)
 
 	def setResistance2(self):
-		setTarget(RESISTANCE_2)
+		self.setTarget(RESISTANCE_2)
 
 	def setResistance3(self):
-		setTarget(RESISTANCE_3)
+		self.setTarget(RESISTANCE_3)
 
 # simple test harness to walk through the various steps.
 def _test(port="/dev/ttyACM0", channel=0x05):
