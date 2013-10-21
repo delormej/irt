38b68e6fcf84476da569ef50f991a72890705023
unknown <jasondel@jasondel-x1.redmond.corp.microsoft.com>
2013-10-21 11:00:49 -0400

    Added USB tracing command

---
+++ ant/trace.cmd
@@ -0,0 +1,31 @@
+
+@REM INSTRUCTIONS HERE: http://msdn.microsoft.com/en-us/library/windows/hardware/hh451296(v=vs.85).aspx
+
+echo off
+@REM ---------------------------------------------------------------------------------------
+@REM UNCOMMENT THE LOGMAN COMMANDS FOR THE FOLLOWING PROVIDERS AS REQUIRED
+@REM Microsoft-Windows-WPD-API                 To log API traffic
+@REM Microsoft-Windows-WPD-MTPClassDriver      To log MTP command, response and datasets
+@REM Microsoft-Windows-WPD-MTPUS               To log USB traffic at WpdMtpUS layer
+@REM Microsoft-Windows-WPD-MTPIP               To log IP traffic at WpdMtpIP layer
+@REM Microsoft-Windows-WPD-MTPBT               To log BT traffic at WpdMtpBt layer
+@REM Microsoft-Windows-USB-USBPORT             To log USB core layer traffic
+@REM Microsoft-Windows-USB-USBHUB              To log USB core layer traffic
+@REM ---------------------------------------------------------------------------------------
+
+@REM Start Logging
+
+logman start  -ets WPD -p Microsoft-Windows-WPD-API            -bs 100 -nb 128 640 -o wpd_trace.etl
+logman update -ets WPD -p Microsoft-Windows-WPD-MTPClassDriver -bs 100 -nb 128 640
+logman update -ets WPD -p Microsoft-Windows-WPD-MTPUS          -bs 100 -nb 128 640
+@REM logman update -ets WPD -p Microsoft-Windows-WPD-MTPIP          -bs 100 -nb 128 640
+@REM logman update -ets WPD -p Microsoft-Windows-WPD-MTPBT          -bs 100 -nb 128 640
+logman update -ets WPD -p Microsoft-Windows-USB-USBPORT        -bs 100 -nb 128 640
+logman update -ets WPD -p Microsoft-Windows-USB-USBHUB         -bs 100 -nb 128 640
+logman update -ets WPD -p Microsoft-Windows-Kernel-IoTrace 0 2
+echo. 
+echo Please run your scenario now and
+pause
+
+@REM Stop logging
+logman stop -ets WPD
