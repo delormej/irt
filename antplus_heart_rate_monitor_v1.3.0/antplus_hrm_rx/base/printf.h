48e11f163b0dcc1046a250116a7d6bd93a1fb6ea
delormej <delormej@hotmail.com>
2013-10-23 10:25:15 -0400

    Minor bug fixes in speed, working.

--- antplus_heart_rate_monitor_v1.3.0/antplus_hrm_rx/base/printf.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PRINTF_H__
-#define __PRINTF_H__
-int printf(const char *format, ...);
-int sprintf(char *out, const char *format, ...);
-#endif
-
--- python/speed.py
+++ python/speed.py
@@ -1,7 +1,7 @@
 import time
-import maestro
+from maestro import *
 
-TICK_CHANNEL=0
+TICK_CHANNEL=1
 TICK_ROLLOVER = 13120
 TICKS_PER_01 = 144.0	# 144 ticks per 0.01 miles
 MS_IN_HOUR = 3600.0
@@ -11,10 +11,13 @@ class speed(object):
 	def __init__(self):
 		self._last_time_ms = 0	# last reading in milliseconds
 		self._last_count = 0	# last tick count, 144 ticks = 0.01 miles
-		self._maestro = maestro()
+		self._maestro = Maestro()
+		self._maestro.open()
+
+	def __del__(self):
+		self._maestro.close()
 	
 	def get_speed(self):
-		
 		count_delta = 0
 		time_delta = 0
 		count = self._get_count()
@@ -24,7 +27,7 @@ class speed(object):
 		if self._last_time_ms == 0:
 			self._last_time_ms = time_ms
 			self._last_count = count
-			return 0
+			return 0.0
 
 		# if current count is less, a rollover occured
 		if count < self._last_count:
@@ -43,12 +46,16 @@ class speed(object):
 		self._last_time_ms = time_ms
 		self._last_count = count
 
+		return speed
+
 	def _get_count(self):
 		return self._maestro.getFastPosition(TICK_CHANNEL)
 
 	def _get_time_ms(self):
 		return time.time()
 
-s = speed()
-while True:
-	s.get_speed()
+def _test(self):
+	s = speed()
+	while True:
+		time.sleep(1.0)
+		print "Speed: " + str(s.get_speed())
