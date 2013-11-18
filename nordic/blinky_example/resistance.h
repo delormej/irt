0973084f39eeb6f36db8ffe30ce8c27a480c152f
delormej <delormej@hotmail.com>
2013-11-17 19:00:45 -0500

    Working C code that responds to buttons pushing II and III to raise and lower resistance

---
+++ nordic/blinky_example/resistance.h
@@ -0,0 +1,20 @@
+#ifndef __RESISTANCE_H__
+#define __RESISTANCE_H__
+
+#include <stdint.h>
+
+#define MAX_RESISTANCE_LEVELS 10
+
+uint32_t RESISTANCE_LEVEL[MAX_RESISTANCE_LEVELS] = { 
+	2107,
+	1273,
+	1272,
+	1252,
+	1212,
+	1150,
+	1068,
+	966,
+	843,
+	699 };
+
+#endif
