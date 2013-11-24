6c2fdd4248021987407b2a517ce79b79125d0fcc
delormej <delormej@hotmail.com>
2013-11-23 19:16:49 -0500

    Added ble s110 stack dependencies.

--- nordic/blinky_example/main.c
+++ nordic/blinky_example/main.c
@@ -39,6 +39,8 @@
 #define PIN_DRUM_REV 				0		// P3 - P0.00 
 #define GPIOTE_MAX_USERS		3
 
+#define DEAD_BEEF 					0xDEADBEEF /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
+
 uint8_t m_resistance_level = 0;
 
 /*****************************************************************************
@@ -68,6 +70,22 @@ void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p
 
     // On assert, the system can only recover with a reset.
     NVIC_SystemReset();
+}
+
+/**@brief Callback function for asserts in the SoftDevice.
+ *
+ * @details This function will be called in case of an assert in the SoftDevice.
+ *
+ * @warning This handler is an example only and does not fit a final product. You need to analyze 
+ *          how your product is supposed to react in case of Assert.
+ * @warning On assert from the SoftDevice, the system can only recover on reset.
+ *
+ * @param[in]   line_num   Line number of the failing ASSERT call.
+ * @param[in]   file_name  File name of the failing ASSERT call.
+ */
+void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
+{
+    app_error_handler(DEAD_BEEF, line_num, p_file_name);
 }
 
 void set_resistance(uint32_t level)
