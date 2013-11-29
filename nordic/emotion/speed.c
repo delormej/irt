78c0f41a0960bf122e9c44634b01d4b8923eb2c1
Jason De Lorme (x1) <delormej@hotmail.com>
2013-11-29 17:26:50 -0500

    Clean up, refactoring and added mph/kmh functions.

--- nordic/emotion/speed.c
+++ nordic/emotion/speed.c
@@ -1,3 +1,11 @@
+/*
+*******************************************************************************
+* 
+* By Jason De Lorme <jjdelorme@yahoo.com>
+* http://www.roadacious.com
+*
+********************************************************************************/
+
 #include "speed.h"
 #include "nrf_sdm.h"
 #include "app_error.h"
@@ -82,10 +90,25 @@ static uint16_t revs_get_count()
 	return revs;
 }
 
-/**@brief		Returns the number of wheel revolutions.
- *
- *@note			Approx. 19 flywheel revolutions per 1 2.07m wheel revolution.
- */
+float get_speed_kmh(uint16_t wheel_revolutions, uint16_t seconds_2048)
+{
+	if (wheel_revolutions == 0 || seconds_2048 == 0)
+		return 0.0f;
+	
+	// Convert mm to meters, divide by revs, multiply by 1,000 for km.
+	float km = ((m_wheel_size*1000) / wheel_revolutions)*1000;
+	// Divide distance by time (multiple by  2048 to get seconds, then seconds in hour).
+	float speed = km / (seconds_2048 * 2048 * 3600);
+	
+	return speed;
+}
+
+float get_speed_mph(uint16_t wheel_revolutions, uint16_t seconds_2048)
+{
+	// Convert km/h to mp/h.
+	return get_speed_kmh(wheel_revolutions, seconds_2048) * 1.609344;
+}
+
 uint16_t get_wheel_revolutions()
 {
 	/*
@@ -99,7 +122,7 @@ uint16_t get_wheel_revolutions()
 	if (revs == 0)
 		return 0;
 	
-	// TODO: Use m_wheel_size to accurately calculatoe 
+	// TODO: Use m_wheel_size to more accurately calculate flywheel to wheel revs.
 	uint16_t flywheel_to_wheel_revs = 19;
 	return ROUNDED_DIV(revs, flywheel_to_wheel_revs);
 }
--- nordic/emotion/speed.h
+++ nordic/emotion/speed.h
@@ -1,3 +1,14 @@
+/*
+*******************************************************************************
+* 
+* By Jason De Lorme <jjdelorme@yahoo.com>
+* http://www.roadacious.com
+*
+* This module is responsible for interacting with the photo sensor measuring 
+* revolutions on the fly wheel and reporting speed/distance based on this.
+*
+********************************************************************************/
+
 #ifndef __REVOLUTIONS_H__
 #define __REVOLUTIONS_H__
 
@@ -23,5 +34,17 @@ void init_speed(uint32_t pin_drum_rev, uint16_t wheel_size_mm);
 /**@brief 	Gets the count of wheel revolutions since the counter started.
  */
 uint16_t get_wheel_revolutions(void);
+
+/**@brief		Returns speed in kilometers per hour (kph) given a period.  The 
+ *					period is defined in 1/2048ths of a second.
+ *					
+ */
+float get_speed_kmh(uint16_t wheel_revolutions, uint16_t seconds_2048);
+
+/**@brief		Returns speed in miles per hour (kph) given a period.  The 
+ *					period is defined in 1/2048ths of a second.
+ *					
+ */
+float get_speed_mph(uint16_t wheel_revolutions, uint16_t seconds_2048);
 
 #endif
