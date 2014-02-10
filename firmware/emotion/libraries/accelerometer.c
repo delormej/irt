64c2ac75822b1fe7cf10dfc5553f7242a1b5528c
Jason De Lorme (x1) <delormej@hotmail.com>
2014-02-10 11:56:49 -0500

    Not powering the device off after BLE timeout and not putting accelerometer to sleep.

--- firmware/emotion/libraries/accelerometer.c
+++ firmware/emotion/libraries/accelerometer.c
@@ -84,7 +84,7 @@ static void enable_interrupt(void)
 	//
 	// Enable Auto-Sleep.
 	//
-	ret = accelerometer_write(REG8652_CTRL_REG2, AUTO_SLEEP_ENABLE);
+	//ret = accelerometer_write(REG8652_CTRL_REG2, AUTO_SLEEP_ENABLE);
 
 	//
 	// Set CTRL_REG3 WAKE_FF_MT bit to 1 to enable motion interrupt.
