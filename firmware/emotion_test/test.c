cfa21a231e0ee46333e2141287bb998a451bed7b
Jason De Lorme (x1) <delormej@hotmail.com>
2014-02-23 10:09:43 -0500

    Added methods to test servo position and power calcs.

--- firmware/emotion_test/test.c
+++ firmware/emotion_test/test.c
@@ -3,12 +3,33 @@
 %comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat""
 (2) build with debug symbols.
 cl test.c /ZI 
+
+
+cl test.c ..\emotion\libraries\power.c ..\emotion\libraries\resistance.c ..\emotion\libraries\nrf_pwm.c /ZI /I..\emotion\libraries\
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <float.h>
+#include "power.h"
+
+#define MAX_RESISTANCE_LEVELS 10						// Maximum resistance levels available.
+#define MIN_RESISTANCE_LEVEL	1500					// Minimum by which there is no longer resistance.
+
+
+static const uint16_t RESISTANCE_LEVEL[MAX_RESISTANCE_LEVELS] = {
+	2107, // 0 - no resistance
+	1300,
+	1225,
+	1150,
+	1075,
+	1000,
+	925,
+	850,
+	775,
+	700}; // Max resistance
+
 
 float get_resistance_pct(uint8_t *buffer)
 {
@@ -73,9 +94,41 @@ int get_addr(my_t** pp_my_t)
 	*pp_my_t = p_my;
 }
 
+static uint16_t calc_servo_pos(float weight_kg, float speed_mps, float force_needed)
+{
+#define SERVO_FORCE_A_SLOPE 			-24.66803762f
+#define SERVO_FORCE_A_INTERCEPT 	1489.635063f
+#define SERVO_FORCE_A_SPEED			 	8.94f
+#define SERVO_FORCE_B_SLOPE 			-26.68991676f
+#define SERVO_FORCE_B_INTERCEPT 	1468.153562f
+#define SERVO_FORCE_B_SPEED			 	4.47f
+
+	float value = (force_needed * SERVO_FORCE_A_SLOPE + SERVO_FORCE_A_INTERCEPT) -
+		(((force_needed * SERVO_FORCE_A_SLOPE + SERVO_FORCE_A_INTERCEPT) -
+		(force_needed * SERVO_FORCE_B_SLOPE + SERVO_FORCE_B_INTERCEPT)) /
+		SERVO_FORCE_A_SPEED - SERVO_FORCE_B_SPEED)*(SERVO_FORCE_A_SPEED - speed_mps);
+
+	// Round the position.
+	uint16_t servo_pos = (uint16_t) value; //  ceil(value);
+
+	// Enforce min/max position.
+	if (servo_pos > RESISTANCE_LEVEL[0])
+		servo_pos = RESISTANCE_LEVEL[0];
+	else if (servo_pos < RESISTANCE_LEVEL[MAX_RESISTANCE_LEVELS - 1])
+		servo_pos = RESISTANCE_LEVEL[MAX_RESISTANCE_LEVELS - 1];
+
+	return servo_pos;
+}
+
 
 int main(int argc, char *argv [])
 {
+	uint16_t position;
+	uint16_t watts;
+	float weight_kg;
+	float speed_mps;
+	float force_needed;
+
 	printf("Press any key.");
 	scanf("Starting...");
 	/*
@@ -84,10 +137,17 @@ int main(int argc, char *argv [])
 	get_addr(pp_my);
 
 	p_my = *pp_my;
-	*/
+
 
 	my_t* p_my	= NULL;
 	get_addr(&p_my);
+*/
+	weight_kg = 79.83219f;
+	speed_mps = 6.71f;
+	force_needed = 13.95185f;
 
-	printf("val: ||%f, %i||", p_my->f, p_my->x);
+	position = calc_servo_pos(weight_kg, speed_mps, force_needed);
+	calc_power2(speed_mps, weight_kg, position, &watts);
+
+	printf("val: %i||%i", position, watts);
 }
