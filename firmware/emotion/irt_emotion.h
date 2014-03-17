6cd1d2fbcd4d7054cb4309a822a24ceb3fe4a09c
delormej <delormej@hotmail.com>
2014-03-17 13:45:06 -0400

    Moving SW rev and device id to the make file so that we can change on build.

--- firmware/emotion/irt_emotion.h
+++ firmware/emotion/irt_emotion.h
@@ -3,11 +3,11 @@
 
 #define DEVICE_NAME                     "E-Motion"                            /**< Name of device. Will be included in the advertising data. */
 #define MANUFACTURER_NAME               "Inside Ride Technologies"            /**< Manufacturer. Will be passed to Device Information Service. */
-#define ANT_DEVICE_NUMBER         		0x01                                         /**< TODO: need a unique Device Number derived from device serial no. */
-#define HW_REVISION                     0x7Fu               									/**< Hardware revision for manufacturer's identification common page. */
-#define MANUFACTURER_ID                 0xAAAAu             									/**< Manufacturer ID for manufacturer's identification common page. */
-#define MODEL_NUMBER                    0x5555u             									/**< Model number for manufacturer's identification common page. */
-#define SW_REVISION                     0xAAu               									/**< Software revision number for product information common page. */
-#define SERIAL_NUMBER                   0xAA55AA55u         									/**< Serial number for product information common page. */
+//#define ANT_DEVICE_NUMBER         		0x01                                  /**< TODO: need a unique Device Number derived from device serial no. */
+#define HW_REVISION                     0x01               					  /**< Hardware revision for manufacturer's identification common page. */
+#define MANUFACTURER_ID                 0xAAAAu             				  /**< Manufacturer ID for manufacturer's identification common page. */
+#define MODEL_NUMBER                    0x01             					  /**< Model number for manufacturer's identification common page. */
+//#define SW_REVISION                     0x02u               				  /**< Software revision number for product information common page. */
+#define SERIAL_NUMBER                   0xAA55AA55u         				  /**< Serial number for product information common page. */
 
 #endif	// IRT_EMOTION_H__
