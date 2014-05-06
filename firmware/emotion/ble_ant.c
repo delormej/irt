97401d5dfcac92fe42ac5d522978a67e751712a9
delormej <delormej@hotmail.com>
2014-05-06 11:45:13 -0400

    Setting bond flag back to 1, this is what now allows Android to connect again.

--- firmware/emotion/ble_ant.c
+++ firmware/emotion/ble_ant.c
@@ -49,7 +49,7 @@
 #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                            /**< Number of attempts before giving up the connection parameter negotiation. */
 
 #define SEC_PARAM_TIMEOUT               30                                           /**< Timeout for Pairing Request or Security Request (in seconds). */
-#define SEC_PARAM_BOND                  0                                            /**< Perform bonding. */
+#define SEC_PARAM_BOND                  1                                            /**< Perform bonding. */
 #define SEC_PARAM_MITM                  0                                            /**< Man In The Middle protection not required. */
 #define SEC_PARAM_IO_CAPABILITIES       BLE_GAP_IO_CAPS_NONE                         /**< No I/O capabilities. */
 #define SEC_PARAM_OOB                   0                                            /**< Out Of Band data not available. */
