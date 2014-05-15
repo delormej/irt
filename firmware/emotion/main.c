31e477a16c0ee35f71670db724958ccb9c9f97f8
Jason De Lorme (x1) <delormej@hotmail.com>
2014-05-14 21:03:14 -0400

    Fixed error reporting to properly use bitmask.

--- firmware/emotion/main.c
+++ firmware/emotion/main.c
@@ -106,7 +106,7 @@ void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p
 				p_file_name, line_num);
 		return;
 	}
-	else if (error_code && IRT_ERROR_AC_BASE_NUM)
+	else if ((error_code & IRT_ERROR_AC_BASE_NUM) == IRT_ERROR_AC_BASE_NUM)
 	{
 		LOG("[MAIN]:app_error_handler WARN: Accelerometer fail, %s line:{%lu}.\r\n",
 				p_file_name,
--- firmware/emotion_test/test.c
+++ firmware/emotion_test/test.c
@@ -22,8 +22,27 @@ cl test.c ..\emotion\libraries\power.c ..\emotion\libraries\resistance.c ..\emot
 #define DM_LOG(...)        /**< Diasbles detailed logs. */
 #endif // DM_DIABLE_LOGS
 
+#define IRT_ERROR_BASE_NUM      (0x80000)       				///< Error base, hopefully well away from anything else.
+#define IRT_ERROR_RC_BASE_NUM   IRT_ERROR_BASE_NUM + (0x100)   	///< Error base for Resistance Control
+
+
+void check(uint32_t err)
+{
+	if ((err & IRT_ERROR_RC_BASE_NUM) == IRT_ERROR_RC_BASE_NUM)
+	{
+		printf("Yes\r\n");
+	}
+	else
+	{
+		printf("No\r\n");
+	}
+}
+
 int main(int argc, char *argv [])
 {
+	check(IRT_ERROR_RC_BASE_NUM);	// 1000 0000 0001 0000 0000
+	check(12213);					//		  10 1111 1011 0101
+
 	DM_LOG("[LOG] Testing\r\n");
 	printf("Test DONE\r\n");
 }
