571093a6c73039a4105307050c875a9b33a2790e
Jason De Lorme (x1) <delormej@hotmail.com>
2014-08-01 14:38:19 -0700

    Trying the unmanged route as I got from Scott Hanselmann's blog.

--- ant/DEMO_NET/Services/SpeedSimulator.cs
+++ ant/DEMO_NET/Services/SpeedSimulator.cs
@@ -10,26 +10,55 @@ namespace ANT_Console.Services
     public class SpeedSimulator : IDisposable
     {
         Timer m_timer;
-        SerialPort m_port;
+        //SerialPort m_port;
+        UnmanagedIRSerialPort m_port;
+        bool m_state = false;
 
         public SpeedSimulator() 
         {
+            /*
             m_port = new SerialPort("COM3", 38400);
+            m_port.RtsEnable = true;
+            m_port.StopBits = StopBits.One;
+            m_port.Parity = Parity.None;
+            m_port.DataBits = 7;
+            m_port.Handshake = Handshake.None;
+
             m_port.Open();
-            
+            */
+
+            m_port = new UnmanagedIRSerialPort("COM3", 115200); // 38400);
+            m_port.Open();
+
             m_timer = new Timer(Simulate_Callback);
         }
 
+        /*
         public SpeedSimulator(SerialPort port)
         {
             m_port = port;
             m_timer = new Timer(Simulate_Callback);
-        }
+        } */
 
         private void Simulate_Callback(object state)
         {
             // Flip the bit.
-            m_port.RtsEnable = !m_port.RtsEnable;
+            //m_port.RtsEnable = !m_port.RtsEnable;
+
+            //lock (this)
+            //{
+                if (m_state)
+                {
+                    m_port.On();
+                }
+                else
+                {
+                    m_port.Off();
+                }
+
+                // Flip the state.
+                m_state = !m_state;
+            //}
         }
 
         public void Simulate(float speed_mps)
@@ -46,8 +75,8 @@ namespace ANT_Console.Services
             // 2 events per flywheel
             long ticks = (long)(((1 / (flyWheelPerSec)) * TimeSpan.TicksPerSecond) / 2);
 
-            //TimeSpan interval = new TimeSpan(ticks);
-            TimeSpan interval = new TimeSpan(0, 0, 0, 0, 8);
+            TimeSpan interval = new TimeSpan(ticks);
+            //TimeSpan interval = new TimeSpan(0, 0, 0, 0, 8);
 
             return interval;
         }
