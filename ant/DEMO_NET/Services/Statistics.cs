d4a3726994ee40baff9b5928bbfef88649b7fd57
Jason De Lorme (x1) <delormej@hotmail.com>
2014-08-22 10:15:16 -0400

    Theil-Sen calculations.

---
+++ ant/DEMO_NET/Services/Statistics.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ANT_Console.Services
+{
+    public static class Statistics
+    {
+        public static double Mean(this IEnumerable<double> list)
+        {
+            return list.Average(); // :-)
+        }
+ 
+        public static double Median(this IEnumerable<double> list)
+        {
+            List<double> orderedList = list
+                .OrderBy(numbers => numbers)
+                .ToList();
+ 
+            int listSize = orderedList.Count;
+            double result;
+ 
+            if (listSize%2 == 0) // even
+            {
+                int midIndex = listSize/2;
+                result = ((orderedList.ElementAt(midIndex - 1) +
+                           orderedList.ElementAt(midIndex))/2);
+            }
+            else // odd
+            {
+                double element = (double) listSize/2;
+                element = Math.Round(element, MidpointRounding.AwayFromZero);
+ 
+                result = orderedList.ElementAt((int) (element - 1));
+            }
+ 
+            return result;
+        }
+ 
+        public static IEnumerable<double> Modes(this IEnumerable<double> list)
+        {
+            var modesList = list
+                .GroupBy(values => values)
+                .Select(valueCluster =>
+                        new
+                            {
+                                Value = valueCluster.Key,
+                                Occurrence = valueCluster.Count(),
+                            })
+                .ToList();
+ 
+            int maxOccurrence = modesList
+                .Max(g => g.Occurrence);
+ 
+            return modesList
+                .Where(x => x.Occurrence == maxOccurrence && maxOccurrence > 1) // Thanks Rui!
+                .Select(x => x.Value);
+        }
+    }
+}
---
+++ ant/DEMO_NET/Services/TheilSen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ANT_Console.Services
+{
+    public static class TheilSen
+    {
+        static ArrayList Calc(List<double> x, List<double> y)
+        {
+
+            int n = x.Count; 
+            //if n < 100 or not sample:
+
+            //        ix = np.argsort( x )
+            
+
+            //slopes = np.empty( n*(n-1)*0.5 )
+            
+
+            return null;
+        }
+
+        private static double Slope(double x_1, double x_2, double y_1, double y_2)
+        {
+            return (1 - 2 * (x_1 > x_2 ? 1 : 0) * ((y_2 - y_1) / Math.Abs((x_2 - x_1))));
+        }
+    }
+}
