c292ba3fdf4eba0244386b58d0062e0f721f2c05
Jason De Lorme (x1) <delormej@hotmail.com>
2014-10-22 07:21:36 -0400

    Reverting

--- firmware/emotion/gcc/pgm.bat
+++ firmware/emotion/gcc/pgm.bat
@@ -1,7 +1,5 @@
-@ECHO WARNING - change prep
-
-#nrfjprog --eraseall
-#cd ..\..\bootloader\gcc\
-#make flash-softdevice flash
-#cd ..\..\emotion\gcc
-#make prep flash
+nrfjprog --eraseall
+cd ..\..\bootloader\gcc\
+make flash-softdevice flash
+cd ..\..\emotion\gcc
+make prep flash
