4da9ecaa0afa74f5fa55d3c5228430ac2c153453
delormej <delormej@hotmail.com>
2014-10-27 15:36:42 -0400

    Added in theil sen and using the force value based on mps of flywheel.

--- python/estimator/find_force.py
+++ python/estimator/find_force.py
@@ -8,9 +8,56 @@ import sys
 from collections import defaultdict
 from itertools import groupby, chain
 import numpy as np, numpy.ma as ma
+import bottleneck 
+import itertools
 
 if len(sys.argv) > 1:
 	input_file_name = sys.argv[1]
+
+def theil_sen(x,y, sample= "auto", n_samples = 1e7):
+    """
+    Computes the Theil-Sen estimator for 2d data.
+    parameters:
+        x: 1-d np array, the control variate
+        y: 1-d np.array, the ind variate.
+        sample: if n>100, the performance can be worse, so we sample n_samples.
+                Set to False to not sample.
+        n_samples: how many points to sample.
+    
+    This complexity is O(n**2), which can be poor for large n. We will perform a sampling
+    of data points to get an unbiased, but larger variance estimator. 
+    The sampling will be done by picking two points at random, and computing the slope,
+    up to n_samples times.
+    
+    """
+    assert x.shape[0] == y.shape[0], "x and y must be the same shape."
+    n = x.shape[0]
+    
+    if n < 100 or not sample:
+        ix = np.argsort( x )
+        slopes = np.empty( n*(n-1)*0.5 )
+        for c, pair in enumerate(itertools.combinations( range(n),2 ) ): #it creates range(n) =( 
+            i,j = ix[pair[0]], ix[pair[1]]
+            slopes[c] = slope( x[i], x[j], y[i],y[j] )
+    else:
+        i1 = np.random.randint(0, n, n_samples)
+        i2 = np.random.randint(0, n, n_samples)
+        slopes = slope( x[i1], x[i2], y[i1], y[i2] )
+        #pdb.set_trace()
+    
+    slope_ = bottleneck.nanmedian( slopes )
+    #find the optimal b as the median of y_i - slope*x_i
+    intercepts = np.empty( n )
+    for c in range(n):
+        intercepts[c] = y[c] - slope_*x[c]
+    intercept_ = bottleneck.median( intercepts )
+
+    return np.array( [slope_, intercept_] )
+        
+def slope( x_1, x_2, y_1, y_2):
+    if np.abs(x_2-x_1) == 0:
+        return np.nan
+    return (1 - 2*(x_1>x_2) )*( (y_2 - y_1)/np.abs((x_2-x_1)) )
 
 def find_seq(speeds, watts, offset):
 	def check_seq(se): # test range [i..se) for a valid sequence
@@ -88,7 +135,8 @@ def main(input_file_name):
 		)
 
 	old_err = np.seterr(divide="ignore", invalid="ignore")
-	forces = watts / (speeds * 0.44704)
+	#forces = watts / (speeds * 0.44704)
+	forces = watts / flywheel_mps
 	np.seterr(**old_err)
 	
 	id2000 = np.fromiter(chain.from_iterable((ids for p, ids in valid_data.items() if p >= 2000)), dtype=int)
@@ -96,7 +144,7 @@ def main(input_file_name):
 	f2000 = forces[id2000]
 	w2000 = watts[id2000]
 	#slope, intercept = np.linalg.lstsq(np.vstack([sp2000, np.ones_like(sp2000)]).T, w2000)[0]
-	slope, intercept = np.linalg.lstsq(np.vstack([speeds_mps[id2000], np.ones_like(speeds_mps[id2000])]).T, w2000)[0]
+	slope, intercept = theil_sen(flywheel_mps[id2000], watts[id2000], False)
 
 	print("slope, intercept")
 	print(slope, intercept)
@@ -115,7 +163,7 @@ def main(input_file_name):
 	for p in pos_list:
 		ids = valid_data[p]
 		if ids:
-			print(p, forces[ids].mean(), (forces[ids] - ((speeds_mps[ids]*slope - intercept)/speeds_mps[ids])).mean())
+			print(p, forces[ids].mean(), (forces[ids] - ((flywheel_mps[ids]*slope - intercept)/flywheel_mps[ids])).mean())
 
 if __name__ == "__main__":
    main(sys.argv[1])
