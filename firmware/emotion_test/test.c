f7c99ff6ee4147669ace9e449d6e71e77108479b
Jason De Lorme (x1) <delormej@hotmail.com>
2014-11-05 21:13:39 -0500

    Test stuff.

--- firmware/emotion_test/test.c
+++ firmware/emotion_test/test.c
@@ -17,8 +17,17 @@ cl test.c ..\emotion\libraries\power.c ..\emotion\libraries\resistance.c ..\emot
 #define FLYWHEEL_ROAD_DISTANCE		0.173f										// Virtual road distance traveled in meters per complete flywheel rev.
 #define FLYWHEEL_TICK_PER_METER		((1.0f / FLYWHEEL_ROAD_DISTANCE) * 24.0f)	// 24 ticks per drum rev.
 
+#define STOPPED(val)  ((val & 0xC000) == 0)
+
 int main(int argc, char *argv [])
 {
-	printf("Flywheel ticks per meter: %.4f\r\n", FLYWHEEL_TICK_PER_METER);
+	uint16_t pos[3] = {
+		1645 | 0x4000,
+		1645 | 0x8000,
+		1645 };
+	
+	for (int i = 0; i < (sizeof(pos)/sizeof(uint16_t)); i++)
+		printf( "pos: %i, result: %#x\r\n", pos[i], STOPPED(pos[i]) );
+
 	return 0;
 }
