246f0ee2718d0b0081dabb9d4cb166fd71ff3543
delormej <delormej@hotmail.com>
2014-12-12 09:14:05 -0500

    Minor refactoring.

--- python/estimator/coastdown.py
+++ python/estimator/coastdown.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import scipy.optimize as spo, scipy.stats as stats 
 
 #input_file_name = "coastdown.csv"
-input_file_name = "C:\\Users\\Jason\\SkyDrive\\InsideRide\\Tech\\Ride Logs\\Paul Malan\\coastdown.csv"
 
 # fit to a 2nd degree polynomial
 def fit_poly2d(x_new, x, y):
@@ -40,36 +39,40 @@ def fit_linear(x_new, x, y):
 	print("slope: %s intercept: %s" % (slope, intercept) )
 	plt.plot(x_new, x_new * slope + intercept, 'g')
 
+def main(file_name):
+	#
+	# load the csv file
+	#
+	time, tick_delta = np.loadtxt(file_name, delimiter=',', skiprows=1,
+							dtype=[('ms', int), ('tick_delta', int)], usecols=[0, 2], unpack=True, comments='"')
 
-# load the csv file
-time, tick_delta = np.loadtxt(input_file_name, delimiter=',', skiprows=1,
-						dtype=[('ms', int), ('tick_delta', int)], usecols=[0, 2], unpack=True, comments='"')
+	# calculate new x/y to represent time in ms since 0 and speed in meters per second
+	y = (time.max() - time) / 1000.0	# seconds until min
+	x = tick_delta * 20 * 0.1115/2		# meters per second
 
-# calculate new x/y to represent time in ms since 0 and speed in meters per second
-y = (time.max() - time) / 1000.0	# seconds until 0
-x = tick_delta * 20 * 0.1115/2		# meters per second
+	# Set axis labels
+	plt.xlabel('Speed (mps)')
+	plt.ylabel('Coastdown Time (seconds)')
 
-# Set axis labels
-plt.xlabel('Speed (mps)')
-plt.ylabel('Coastdown Time (seconds)')
+	# plot actual values
+	plt.plot(x, y)
+	plt.ylim(ymin=0)
+	plt.xlim(xmax=x.max())
 
-# plot actual values
-plt.plot(x, y)
-plt.ylim(ymin=0)
-plt.xlim(xmax=x.max())
+	# if I wanted to reverse the axis visualy, also need to adjust min/max for this.
+	#plt.gca().invert_yaxis()
 
-# if I wanted to reverse the axis visualy, also need to adjust min/max for this.
-#plt.gca().invert_yaxis()
+	# come up with even set of new x's - makes up for missing data points, etc...
+	x_new = np.linspace(x[0], x[-1], len(x))
+	fp = fit_power(x_new, x, y)
+	f2d = fit_poly2d(x_new, x, y)
+	fit_linear(x_new, x, y)
 
-# come up with even set of new x's - makes up for missing data points, etc...
-x_new = np.linspace(x[0], x[-1], len(x))
-fp = fit_power(x_new, x, y)
-f2d = fit_poly2d(x_new, x, y)
-fit_linear(x_new, x, y)
+	# print the formula
+	plt.text(x.max() * 0.05, y.max() * 0.95, fp, fontsize=8, color='y')
+	plt.text(x.max() * 0.05, y.max() * 0.90, f2d, fontsize=8, color='r')
 
-# print the formula
-plt.text(x.max() * 0.05, y.max() * 0.95, fp, fontsize=8, color='y')
-plt.text(x.max() * 0.05, y.max() * 0.90, f2d, fontsize=8, color='r')
+	# show the chart
+	plt.show()
 
-# show the chart
-plt.show()
+main("C:\\Users\\Jason\\SkyDrive\\InsideRide\\Tech\\Ride Logs\\Tariq\\20141206_Calibration\\coastdown.csv")
