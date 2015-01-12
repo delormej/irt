c10874b88f30c2b340fc8cce469450b0080d520b
Jason De Lorme (x1) <delormej@hotmail.com>
2015-01-12 17:39:47 -0500

    Implemented IEEE754 float parsing

--- firmware/emotion_test/test.c
+++ firmware/emotion_test/test.c
@@ -9,6 +9,7 @@ cl test.c ..\emotion\libraries\power.c ..\emotion\libraries\resistance.c ..\emot
 */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
@@ -16,29 +17,39 @@ cl test.c ..\emotion\libraries\power.c ..\emotion\libraries\resistance.c ..\emot
 #include <float.h>
 #include <windows.h>
 
-#define LED_FRONT_GREEN_PIN		18
-#define LED_FRONT_RED_PIN		19
-#define LED_BACK_GREEN_PIN		20
-#define LED_BACK_RED_PIN		21
+static float float_from_buffer(uint32_t* p_buffer)
+{
+	// Signed int contains the fraction, starting at byte 2.
+	// IEEE754 binary32 format for float.
+	// Sign 1 bit, exponent (8 bits), fraction (23 bits)
+	// 0 00000000 00000000000000000000000
 
-#define LED_FRONT_GREEN			0
-#define LED_FRONT_RED			1
-#define LED_BACK_GREEN			2
-#define LED_BACK_RED			3
+	bool sign = (*p_buffer) >> 31;
 
+	// mask out 23 least significant bits for the fraction value.
+	int32_t value = ((*p_buffer) & 0xFFFFFF); // | (sign << 31);
+	// mask out most significant bits 2 - 8
+	uint8_t exponent = ((*p_buffer) & 0x7F000000) >> 24;
+
+	float fraction = value / pow(2, exponent);
+
+	if (sign)
+	{
+		fraction *= -1;
+	}
+
+	printf("val:%i, exp:%i, sign:%i, fraction:%.7f\r\n", 
+		value, exponent, sign, fraction);
+
+	return 0.0f;
+}
 
 int main(int argc, char *argv [])
 {
-	uint8_t count = 0;
-
-	while (-1)
-	{
-		count = ++count & 3u;
-		printf("count: %u\r\n", count);
-		
-		Sleep(250);
-	}
-	
+	//int32_t value = -5767294;
+	uint32_t value = ((1 << 31) | 5767294 | 385875968); /* 23 << 31 bits*/
+	printf("Original = %i\r\n", value);
+	float_from_buffer(&value);
 
 	return 0;
 }
