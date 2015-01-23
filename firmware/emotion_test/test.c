8927fece3fe337cbfccc2da537fdcbd7494c22d7
delormej <delormej@hotmail.com>
2015-01-23 13:51:17 -0500

    Updating polynomials.

--- firmware/emotion_test/test.c
+++ firmware/emotion_test/test.c
@@ -18,7 +18,7 @@ cl test.c ..\emotion\libraries\power.c ..\emotion\libraries\resistance.c ..\emot
 #include <windows.h>
 
 #define BASE_SPEED_MPS		6.7056f
-#define MAX_POSITION		1400	// temporary
+#define MAX_POSITION		1500	// temporary
 #define MIN_POSITION		800
 #define MAX_RECURSION		MAX_POSITION - MIN_POSITION
 #define START_SKIP			100
@@ -31,12 +31,22 @@ float speed_slope_from_position(uint16_t position)
 {
 	float slope;
 
-	slope = (
+	// TRY THIS:
+	// y = -2.032933219·10-12 x5 + 1.066543186·10-8 x4 - 2.14085667·10-5 x3 + 2.031246676·10-2 x2 - 9.039501598 x + 1555.297882
+
+	/*slope = (
 		6.151574806f * powf(10, -7)
 		* powf(position, 3)
 		- 2.049592878f * powf(10, -3)
 		* powf(position, 2)
-		+ 2.13907033f * position - 658.5540447f);
+		+ 2.13907033f * position - 658.5540447f);*/
+
+	slope = (
+		-2.032933219f * powf(10, -12) * powf(position, 5)
+		+ 1.066543186 * powf(10, -8) * powf(position, 4)
+		- 2.14085667 * powf(10, -5) * powf(position, 3)
+		+ 2.031246676 * powf(10, -2) * powf(position, 2)
+		- 9.039501598 * position + 1555.297882f);
 
 	return slope;
 }
@@ -49,11 +59,20 @@ uint16_t watts_from_position_base(uint16_t position)
 {
 	uint16_t base_watts;
 
+	// TRY THIS:
+	// cubic model at 15 mph:
+	// y = 1.381313131·10-6 x3 - 4.520887446·10-3 x2 + 4.257886003 x - 870.2186148
+
 	// Step 1. Solve for watts @ base speed (6.7056 mps).
-	base_watts = (
+	/*base_watts = (
 		2.333333333f * powf(10, -6) * powf(position, 3)
 		- 0.0078375f * powf(position, 2)
 		+ 8.044166667f * position - 2277);
+		*/
+	base_watts = (
+		1.381313131f * powf(10, -6) * powf(position, 3)
+		- 4.520887446f * powf(10, -3) * powf(position, 2)
+		+ 4.257886003f * position - 870.2186148f);
 
 	return base_watts;
 }
@@ -138,14 +157,20 @@ int main(int argc, char *argv [])
 	uint16_t input;
 	uint16_t position;
 	uint16_t watts;
+	float speed;
 
 	printf("Enter watt target: ");
 	scanf("%i", &input);
 
-	position = position_from_watts(input, 17.6f * 0.44704f);
-	watts = (uint16_t) watts_from_position(position, 17.6f * 0.44704f);
+	printf("Enter speed (mph): ");
+	scanf("%f", &speed);
 
-	printf("Position: %i, Watts: %i\r\n", position, watts);
+	speed = speed * 0.44704f;
+
+	position = position_from_watts(input, speed);
+	watts = (uint16_t) watts_from_position(position, speed);
+
+	printf("Position: %i, Watts: %i for speed: %.4f mps\r\n", position, watts, speed);
 	
 	return 0;
 }
