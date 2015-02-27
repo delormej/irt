3cce055a156d7bdaeeaf9faeea7779ffc78bde7b
delormej <delormej@hotmail.com>
2015-02-26 23:04:27 -0500

    Now fitting to power curve.

--- calibration/CurveFit.cs
+++ calibration/CurveFit.cs
@@ -11,8 +11,6 @@ namespace Calibration
     /// </summary>
     internal class DecelerationFit
     {
-        bool m_fit;
-
         alglib.barycentricinterpolant m_interpolant;
         double[] m_speedMps, m_coastdownSeconds;  // x, y coordinates.
 
@@ -20,7 +18,6 @@ namespace Calibration
         {
             m_speedMps = speedMps;
             m_coastdownSeconds = coastdownSeconds;
-            m_fit = false;
         }
 
         /// <summary>
@@ -30,10 +27,6 @@ namespace Calibration
         /// <returns></returns>
         public double Rate(double speed_mps)
         {
-            if (!m_fit)
-            {
-                Fit();
-            }
             return alglib.barycentriccalc(m_interpolant, speed_mps);
         }
 
@@ -42,7 +35,7 @@ namespace Calibration
         /// Smoothes and fits raw coast down data to a curve.  This method must 
         /// be called before evaluating any deceleration rates.
         /// </summary>
-        private void Fit()
+        public void Fit()
         {
             // internal implementation of curve fitting.
             int info = 0;
@@ -58,15 +51,28 @@ namespace Calibration
             {
                 throw new CoastdownException("Unable to fit deceleration curve.");
             }
-
-            m_fit = true;
         }
     }
 
     internal class PowerFit
     {
-        public PowerFit(DecelerationFit decelFit)
+        private double[] m_coeff = { 0.0, 0.0 };
+        private DecelerationFit m_decelFit;
+        private double m_inertia;
+
+        public PowerFit(DecelerationFit decelFit, double stableSpeedMps, double stableWatts)
         {
+            m_decelFit = decelFit;
+            m_inertia = MomentOfInteria(stableSpeedMps, stableWatts);
+        }
+
+        public double Drag { get { return m_coeff[0]; } }
+        
+        public double RollingResistance { get { return m_coeff[1]; } }
+
+        public double Watts(double speedMps)
+        {
+            return fit_drag_rr(speedMps, Drag, RollingResistance);
         }
 
         /// <summary>
@@ -75,15 +81,75 @@ namespace Calibration
         /// <param name="speed_mps"></param>
         /// <param name="watts"></param>
         /// <returns></returns>
-        private double MomentOfInteria(double speed_mps, double watts)
+        private double MomentOfInteria(double speedMps, double watts)
         {
             // Where P = F*v, F=ma
+
+            double f = watts / speedMps;
+            double a = m_decelFit.Rate(speedMps);
 
             // Get the rate of deceleration (a) for a given velocity.
             
             // Solve for m = f/a
-            
-            return 0.0;
+            return f / a;
+        }
+
+        public void Fit()
+        {
+            int info;
+            double[,] speed;
+            double[] watts;
+
+            GeneratePowerData(out speed, out watts);
+
+            alglib.lsfitstate state;
+            alglib.lsfitreport report;
+            alglib.lsfitcreatef(speed, watts, m_coeff, 0.0001, out state);
+            alglib.lsfitfit(state, fit_func, null, null);
+            alglib.lsfitresults(state, out info, out m_coeff, out report);
+        }
+
+        private void GeneratePowerData(out double[,] speed, out double[] watts)
+        {
+            speed = new double[30, 1];
+            watts = new double[30];
+
+            int ix = 0;
+            // Generate power data.
+            foreach (var i in Enumerable.Range(5, 30))
+            {
+                double v = i * 0.44704;
+                double a = m_decelFit.Rate(v);
+                double f = m_inertia * a;
+                
+                speed[ix, 0] = v;
+                watts[ix] = f * v;
+
+                ix++;
+            }
+        }
+
+        static double fit_drag_rr(double v, double K, double rr)
+        {
+            double p = K * Math.Pow(v, 2) + ((v * rr)); // / contact_patch());
+            // p = Kv^2 + rrv + c
+
+            return p;
+        }
+
+        static void fit_func(double[] c, double[] x, ref double func, object obj)
+        {
+            func = fit_drag_rr(x[0], c[0], c[1]);
+        }
+
+        static double contact_patch()
+        {
+            //# 'Using this method: http://bikeblather.blogspot.com/2013/02/tire-crr-testing-on-rollers-math.html
+            double wheel_diameter_cm = 210.7 / Math.PI;
+            double drum_diameter_cm = 26 / Math.PI;
+            double patch = Math.Pow((1 / (1 + (wheel_diameter_cm / drum_diameter_cm))), 0.7);
+
+            return patch;
         }
     }
 
@@ -228,7 +294,9 @@ namespace Calibration
 
             // Calculate the deceleration.
             m_decelFit = new DecelerationFit(data.SpeedMps, data.CoastdownSeconds);
-            m_powerFit = new PowerFit(m_decelFit);
+            m_decelFit.Fit();
+            m_powerFit = new PowerFit(m_decelFit, stableSpeed, stableWatts);
+            m_powerFit.Fit();
 
             if (Drag < 0.0)
             {
@@ -245,21 +313,21 @@ namespace Calibration
         /// <summary>
         /// Calculated coefficient of Drag.
         /// </summary>
-        public double Drag { get { return 0; } }
+        public double Drag { get { return m_powerFit.Drag; } }
 
         /// <summary>
         /// Calculated coefficient of Rolling Resistance.
         /// </summary>
-        public double RollingResistance { get { return 0; } }
+        public double RollingResistance { get { return m_powerFit.RollingResistance; } }
 
         /// <summary>
         /// Calculate the rate of deceleration for a given speed.
         /// </summary>
         /// <param name="speed_mps"></param>
         /// <returns></returns>
-        public double Deceleration(double speed_mps)
+        public double Deceleration(double speedMps)
         {
-            return m_decelFit.Rate(speed_mps);
+            return m_decelFit.Rate(speedMps);
         }
 
         /// <summary>
@@ -267,9 +335,9 @@ namespace Calibration
         /// </summary>
         /// <param name="speed_mps"></param>
         /// <returns></returns>
-        public double Watts(double speed_mps)
+        public double Watts(double speedMps)
         {
-            return 0.0;
+            return m_powerFit.Watts(speedMps);
         }
 
         /// <summary>
