52ede541cd50df80a44e1fa0f4671228c5d942b8
delormej <delormej@hotmail.com>
2015-03-17 08:46:50 -0400

    Whitespace.

--- firmware/emotion/libraries/irt_common.h
+++ firmware/emotion/libraries/irt_common.h
@@ -22,8 +22,6 @@ All rights reserved.
 #define FACTORY_SETTINGS_BASE		(((uint8_t*)(NRF_UICR)) + 0x080) 	// Should be NRF_UICR->CUSTOMER[0] or 0x10001080 as per nRF51 Series Manual v3.0
 #define FEATURES 					((uint16_t *) FACTORY_SETTINGS_BASE)
 
-
-
 #define	GRAVITY						9.81f								// Coefficent of gravity.
 #define	MATH_PI						3.14159f
 
