137001c9de7bfbeddfdcf78c0c5ea6c731a6232d
delormej <delormej@hotmail.com>
2015-03-20 20:17:41 -0400

    Testing slope method.

--- firmware/emotion_test/test.c
+++ firmware/emotion_test/test.c
@@ -14,21 +14,45 @@ cl test.c ../emotion/libraries/math/acosf.c ../emotion/libraries/math/sqrtf.c ..
 #include <stdint.h>
 #include <string.h>
 #include <float.h>
+#include <math.h>
 
-typedef struct speed_event_s
+float magnet_watts(float speed_mps, uint16_t position)
 {
-	uint16_t	event_time_2048;											// Event time in 1/2048s.
-	uint16_t	accum_flywheel_ticks;										// Currently 2 ticks per flywheel rev.
-} speed_event_t;
+	//
+	// 1. Determine the slope
+	//
 
-static int speed_calc_mps(speed_event_t first, speed_event_t last)
-{
-	return first.event_time_2048 + last.event_time_2048;
+	// Solve for slope (x: position, y: slope) where slope is the slope of (x: speed(mps), y: watts)
+	// y = 9E-07x3 - 0.0029x2 + 2.89x - 689.51
+	float slope = 9E-07 * pow(position, 3) - 0.0029 * pow(position, 2) + (2.89 * position) - 689.51;
+
+	return slope;
+
+	//
+	// 2. Determine the intercept
+	//
+
+	// Watts @ 4.4704 mps (10 mph)
+	// y = 3E-07x3 - 0.0011x2 + 1.1201x - 291.52
+	float intercept = 3E-07 * pow(position, 3) - 0.0011 * pow(position, 2) + (1.1201 * position) - 291.52;
+	
+	//
+	// 3. Calculate watts for a given speed/position
+	//
+	// y = mx + b
+	float watts = slope * speed_mps + intercept;
+	
+	return watts;
 }
+
+uint16_t magnet_position(float speed_mps, float mag_watts)
+{
+	return 0;
+}
+
 
 int main(int argc, char *argv [])
 {
-
-	printf("total: %u\r\n", UINT32_MAX);
+	printf("watts: %.2f\r\n", magnet_watts(7.15264f, 1200));
 	return 0;
 }
