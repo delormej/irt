ceaea7ccf2d7691d64936adbe5103a39ca9f437e
Jason De Lorme (x1) <delormej@hotmail.com>
2015-04-13 09:23:21 -0400

    Adding settings converter.

---
+++ ant/ANT_IRT_GUI/SettingsConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace IRT_GUI
+{
+    public class SettingsConverter 
+    {
+
+        void UpdateSettings(ushort settings)
+        {
+            ExecuteOnUI(() =>
+            {
+                for (int i = 0; i < chkLstSettings.Items.Count; i++)
+                {
+                    Settings setting = GetSetting(i);
+
+                    bool check = ((settings & (ushort)setting) == (ushort)setting);
+                    chkLstSettings.SetItemChecked(i, check);
+                }
+            });
+        }
+
+        private Settings GetSetting(int index)
+        {
+            // Get the setting
+            Settings setting;
+            Enum.TryParse<Settings>(chkLstSettings.Items[index].ToString(), out setting);
+            return setting;
+        }
+
+    }
+}
