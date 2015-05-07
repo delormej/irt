ea3401cc42584f012ff0d6c594643e83a5aa41e7
delormej <delormej@hotmail.com>
2015-05-07 16:28:50 -0400

    Reorient design for scalability of the coastdown form.

--- calibration/Controls/CoastdownForm.Designer.cs
+++ calibration/Controls/CoastdownForm.Designer.cs
@@ -36,7 +36,6 @@
             this.label4 = new System.Windows.Forms.Label();
             this.btnApply = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.chartCoastdown = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label3 = new System.Windows.Forms.Label();
             this.lblStableSeconds = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -45,24 +44,23 @@
             this.txtStableWatts = new System.Windows.Forms.TextBox();
             this.txtDrag = new System.Windows.Forms.TextBox();
             this.txtRR = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.coastdownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.evaluateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.recalculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.chartCoastdown)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.chartCoastdown = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCoastdown)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(273, 62);
+            this.label2.Location = new System.Drawing.Point(273, 486);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 13);
             this.label2.TabIndex = 1;
@@ -70,10 +68,10 @@
             // 
             // label4
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(273, 85);
+            this.label4.Location = new System.Drawing.Point(273, 509);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(113, 13);
             this.label4.TabIndex = 3;
@@ -102,29 +100,12 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // chartCoastdown
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chartCoastdown.ChartAreas.Add(chartArea1);
-            this.chartCoastdown.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.chartCoastdown.Legends.Add(legend1);
-            this.chartCoastdown.Location = new System.Drawing.Point(3, 16);
-            this.chartCoastdown.Name = "chartCoastdown";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chartCoastdown.Series.Add(series1);
-            this.chartCoastdown.Size = new System.Drawing.Size(487, 405);
-            this.chartCoastdown.TabIndex = 7;
-            this.chartCoastdown.Text = "chart1";
-            // 
             // label3
             // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Location = new System.Drawing.Point(12, 486);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(80, 13);
             this.label3.TabIndex = 10;
@@ -132,9 +113,9 @@
             // 
             // lblStableSeconds
             // 
-            this.lblStableSeconds.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblStableSeconds.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.lblStableSeconds.AutoSize = true;
-            this.lblStableSeconds.Location = new System.Drawing.Point(154, 38);
+            this.lblStableSeconds.Location = new System.Drawing.Point(154, 462);
             this.lblStableSeconds.Name = "lblStableSeconds";
             this.lblStableSeconds.Size = new System.Drawing.Size(13, 13);
             this.lblStableSeconds.TabIndex = 9;
@@ -142,10 +123,10 @@
             // 
             // label6
             // 
-            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 38);
+            this.label6.Location = new System.Drawing.Point(12, 462);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(96, 13);
             this.label6.TabIndex = 8;
@@ -153,10 +134,10 @@
             // 
             // label5
             // 
-            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 85);
+            this.label5.Location = new System.Drawing.Point(12, 509);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(118, 13);
             this.label5.TabIndex = 12;
@@ -164,24 +145,24 @@
             // 
             // txtStableSpeed
             // 
-            this.txtStableSpeed.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtStableSpeed.Location = new System.Drawing.Point(157, 82);
+            this.txtStableSpeed.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtStableSpeed.Location = new System.Drawing.Point(157, 506);
             this.txtStableSpeed.Name = "txtStableSpeed";
             this.txtStableSpeed.Size = new System.Drawing.Size(75, 20);
             this.txtStableSpeed.TabIndex = 14;
             // 
             // txtStableWatts
             // 
-            this.txtStableWatts.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtStableWatts.Location = new System.Drawing.Point(157, 59);
+            this.txtStableWatts.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtStableWatts.Location = new System.Drawing.Point(157, 483);
             this.txtStableWatts.Name = "txtStableWatts";
             this.txtStableWatts.Size = new System.Drawing.Size(75, 20);
             this.txtStableWatts.TabIndex = 13;
             // 
             // txtDrag
             // 
-            this.txtDrag.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtDrag.Location = new System.Drawing.Point(405, 59);
+            this.txtDrag.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtDrag.Location = new System.Drawing.Point(405, 483);
             this.txtDrag.Name = "txtDrag";
             this.txtDrag.ReadOnly = true;
             this.txtDrag.Size = new System.Drawing.Size(75, 20);
@@ -189,27 +170,12 @@
             // 
             // txtRR
             // 
-            this.txtRR.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtRR.Location = new System.Drawing.Point(405, 82);
+            this.txtRR.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtRR.Location = new System.Drawing.Point(405, 506);
             this.txtRR.Name = "txtRR";
             this.txtRR.ReadOnly = true;
             this.txtRR.Size = new System.Drawing.Size(75, 20);
             this.txtRR.TabIndex = 19;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.groupBox1.Controls.Add(this.chartCoastdown);
-            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.groupBox1.Location = new System.Drawing.Point(11, 108);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(493, 424);
-            this.groupBox1.TabIndex = 21;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Chart";
             // 
             // menuStrip1
             // 
@@ -257,6 +223,25 @@
             this.resetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.resetToolStripMenuItem.Text = "Re&set";
             // 
+            // chartCoastdown
+            // 
+            this.chartCoastdown.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chartCoastdown.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartCoastdown.Legends.Add(legend1);
+            this.chartCoastdown.Location = new System.Drawing.Point(0, 27);
+            this.chartCoastdown.Name = "chartCoastdown";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartCoastdown.Series.Add(series1);
+            this.chartCoastdown.Size = new System.Drawing.Size(516, 419);
+            this.chartCoastdown.TabIndex = 7;
+            this.chartCoastdown.Text = "chart1";
+            // 
             // CoastdownForm
             // 
             this.AcceptButton = this.btnApply;
@@ -264,7 +249,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(516, 573);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chartCoastdown);
             this.Controls.Add(this.txtDrag);
             this.Controls.Add(this.txtRR);
             this.Controls.Add(this.txtStableWatts);
@@ -283,10 +268,9 @@
             this.MinimumSize = new System.Drawing.Size(532, 612);
             this.Name = "CoastdownForm";
             this.Text = "Coastdown";
-            ((System.ComponentModel.ISupportInitialize)(this.chartCoastdown)).EndInit();
-            this.groupBox1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCoastdown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -298,7 +282,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnApply;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chartCoastdown;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblStableSeconds;
         private System.Windows.Forms.Label label6;
@@ -307,12 +290,12 @@
         private System.Windows.Forms.TextBox txtStableWatts;
         private System.Windows.Forms.TextBox txtDrag;
         private System.Windows.Forms.TextBox txtRR;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem coastdownToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem evaluateToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem recalculateToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartCoastdown;
     }
 }
