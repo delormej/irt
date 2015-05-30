de0140610b45fd22d344058472bd79f2851f926e
delormej <delormej@hotmail.com>
2015-05-29 20:19:10 -0400

    Adding missing file.

---
+++ calibration/MagnetCalibration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IRT.Calibration
+{
+    public delegate void MagnetCalibrationEventHandler(object sender, MagnetCalibrationEventArgs e);
+
+    public class MagnetCalibration
+    {
+        public MagnetCalibration()
+        {
+            LowSpeedFactors = new float[4];
+            HighSpeedFactors = new float[4];
+        }
+
+        public float LowSpeedMps;
+        public float HighSpeedMps;
+
+        public float[] LowSpeedFactors;
+        public float[] HighSpeedFactors;
+    }
+
+    public class MagnetCalibrationEventArgs : EventArgs
+    {
+        public MagnetCalibration Calibration;
+
+        public MagnetCalibrationEventArgs(MagnetCalibration calibration)
+        {
+            this.Calibration = calibration;
+        }
+    }
+}
