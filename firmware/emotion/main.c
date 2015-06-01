5f9876aab7999e2403f1453f16df253ed55e1dbd
Jason De Lorme (x1) <delormej@hotmail.com>
2015-06-01 13:00:53 -0400

    Fixed issue with BLE sending more info in the standard level packet which was causing the ack to have extra bits.

--- firmware/emotion/main.c
+++ firmware/emotion/main.c
@@ -1328,6 +1328,7 @@ static void on_set_resistance(rc_evt_t rc_evt)
 	switch (rc_evt.operation)
 	{
 		case RESISTANCE_SET_STANDARD:
+			value = (uint8_t)value;
 			resistance_level_set(value);
 			break;
 			
