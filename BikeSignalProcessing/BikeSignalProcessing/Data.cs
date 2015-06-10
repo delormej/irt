90ae6087b4ab77f3555de9945f559d8e21a649c0
Jason De Lorme (x1) <delormej@hotmail.com>
2015-06-10 17:22:56 -0400

    Initial checkin of signal processing for power data in client.

---
+++ BikeSignalProcessing/BikeSignalProcessing/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Statistics;
+
+namespace BikeSignalProcessing
+{
+    public class Data
+    {
+        public double[] RawPower;
+
+        public double[] SmoothedPower;
+
+        public double[] Power5secMA;
+
+        public double[] Power10secMA;
+
+        public Data(string filename)
+        {
+            RawPower = (double[])IrtCsvFactory.Open(filename);
+            SmoothedPower = PowerSmoothing.Smooth(RawPower);
+            Power5secMA = PowerSmoothing.MovingAverage(SmoothedPower, 5);
+            Power10secMA = PowerSmoothing.MovingAverage(SmoothedPower, 10);
+        }
+
+        public double StandardDeviation(int start, int end)
+        {
+            double[] sample = new double[end - start];
+            Array.Copy(SmoothedPower, start, sample, 0, end - start);
+
+            return Statistics.StandardDeviation(sample);
+        }
+    }
+}
---
+++ BikeSignalProcessing/BikeSignalProcessing/PowerSmoothing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Filtering;
+using MathNet.Filtering.Median;
+
+namespace BikeSignalProcessing
+{
+    public class PowerSmoothing
+    {
+        public static double[] Smooth(double[] data)
+        {
+            double[] filteredData;
+
+            //var filter = OnlineFilter.CreateLowpass(ImpulseResponse.Finite, 30, 1);
+            var filter = OnlineFilter.CreateDenoise();
+            filteredData = filter.ProcessSamples(data);
+
+            return filteredData;
+        }
+
+        public static double[] MovingAverage(double[] data, int duration)
+        {
+            OnlineMedianFilter filter = new OnlineMedianFilter(duration);
+            return filter.ProcessSamples(data);
+        }
+    }
+}
