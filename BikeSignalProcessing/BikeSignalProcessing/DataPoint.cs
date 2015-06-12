0eb4cf613dfb9e70a918cef1852a00645db5736d
Jason De Lorme (x1) <delormej@hotmail.com>
2015-06-12 06:27:59 -0400

    Missing file.

---
+++ BikeSignalProcessing/BikeSignalProcessing/DataPoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeSignalProcessing
+{
+    public class DataPoint
+    {
+        public int Seconds { get; set; }
+
+        public double PowerWatts { get; set; }
+
+        public double SpeedMph { get; set; }
+
+        public int ServoPosition { get; set; }
+
+        public double SmoothedPowerWatts { get; set; }
+
+        public double SmoothedSpeedMph { get; set; }
+    }
+
+    public class Segment
+    {
+        public int Start;
+        public int End;
+        public double StdDev;
+        public double AveragePower;
+
+        public Segment()
+        {
+            Start = 0;
+            End = 0;
+        }
+
+        public Segment Copy()
+        {
+            return this.MemberwiseClone() as Segment;
+        }
+    }
+}
