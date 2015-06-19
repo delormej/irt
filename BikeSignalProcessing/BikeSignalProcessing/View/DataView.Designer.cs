9e4c3d58bcb56653d808d8a274d24058f28623a1
Jason De Lorme (x1) <delormej@hotmail.com>
2015-06-18 20:13:46 -0400

    Added speed/power calculation display on the data page.

--- BikeSignalProcessing/BikeSignalProcessing/View/DataView.Designer.cs
+++ BikeSignalProcessing/BikeSignalProcessing/View/DataView.Designer.cs
@@ -31,6 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataView));
             this.dgvSegments = new System.Windows.Forms.DataGridView();
             this.dgvMagnetFit = new System.Windows.Forms.DataGridView();
+            this.txtSpeedMph = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWatts = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSegments)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMagnetFit)).BeginInit();
             this.SuspendLayout();
@@ -50,16 +54,53 @@
             this.dgvMagnetFit.AllowUserToAddRows = false;
             this.dgvMagnetFit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvMagnetFit.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dgvMagnetFit.Location = new System.Drawing.Point(0, 292);
+            this.dgvMagnetFit.Location = new System.Drawing.Point(0, 305);
             this.dgvMagnetFit.Name = "dgvMagnetFit";
-            this.dgvMagnetFit.Size = new System.Drawing.Size(698, 263);
+            this.dgvMagnetFit.Size = new System.Drawing.Size(698, 250);
             this.dgvMagnetFit.TabIndex = 1;
+            // 
+            // txtSpeedMph
+            // 
+            this.txtSpeedMph.Location = new System.Drawing.Point(78, 279);
+            this.txtSpeedMph.Name = "txtSpeedMph";
+            this.txtSpeedMph.Size = new System.Drawing.Size(54, 20);
+            this.txtSpeedMph.TabIndex = 2;
+            this.txtSpeedMph.Text = "15.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Speed (mph)";
+            // 
+            // txtWatts
+            // 
+            this.txtWatts.Location = new System.Drawing.Point(191, 279);
+            this.txtWatts.Name = "txtWatts";
+            this.txtWatts.Size = new System.Drawing.Size(54, 20);
+            this.txtWatts.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Watts";
             // 
             // DataView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(698, 555);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWatts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSpeedMph);
             this.Controls.Add(this.dgvMagnetFit);
             this.Controls.Add(this.dgvSegments);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -68,6 +109,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvSegments)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMagnetFit)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +117,9 @@
 
         private System.Windows.Forms.DataGridView dgvSegments;
         private System.Windows.Forms.DataGridView dgvMagnetFit;
+        private System.Windows.Forms.TextBox txtSpeedMph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWatts;
+        private System.Windows.Forms.Label label2;
     }
 }
--- BikeSignalProcessing/BikeSignalProcessing/View/DataView.cs
+++ BikeSignalProcessing/BikeSignalProcessing/View/DataView.cs
@@ -1,6 +1,7 @@
 ﻿using BikeSignalProcessing.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -18,6 +19,27 @@ namespace BikeSignalProcessing.View
 
             dgvSegments.DataSource = data.StableSegments;
             dgvMagnetFit.DataSource = data.MagnetFits;
+
+            dgvMagnetFit.RowEnter += DgvMagnetFit_RowEnter;
+        }
+
+        private void DgvMagnetFit_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            Collection<MagnetFit> data = dgvMagnetFit.DataSource as
+                Collection<MagnetFit>;
+
+            if (data != null)
+            {
+                MagnetFit fit = data[e.RowIndex];
+                double speedMph;
+                double.TryParse(txtSpeedMph.Text, out speedMph);
+
+                if (speedMph > 0)
+                {
+                    double power = speedMph * fit.Slope + fit.Intercept;
+                    txtWatts.Text = power.ToString("N1");
+                }
+            }
         }
     }
 }
