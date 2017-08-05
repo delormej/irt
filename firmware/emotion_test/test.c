b9aca87375593134a0e17dd898e2444936bed23a
Jason De Lorme <jjdelorme@yahoo.com>
2017-08-05 16:06:16 -0400

    Test code.

--- firmware/emotion_test/test.c
+++ firmware/emotion_test/test.c
@@ -12,75 +12,91 @@ cl test.c ../emotion/libraries/speed_event_fifo.c  /I . /I ../emotion/libraries 
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
-#include <float.h>
-#include <math.h>
-#include "speed_event_fifo.h"
 
-//
-// If speed.h is not included (for test harness)
-//
-#ifndef __REVOLUTIONS_H__
+typedef enum
+{
+	OFF = 0,
+	SOLID	= 0b11111111,
+	SLOW_BLINK = 0b11110000,
+	FAST_BLINK = 0b10000000,
+	FAST_BLINK_2 = 0b10100000,
+	FAST_BLINK_3 = 0b10101000
+} blink_pattern_e;
+
+typedef enum
+{
+	LED_FRONT_GREEN	=		0,
+	LED_FRONT_RED =			1,
+	LED_BACK_GREEN =		2,
+	LED_BACK_RED =			3
+} led_e;
+
 typedef struct
 {
-	uint32_t	event_time_2048;											// Event time in 1/2048s.
-	uint16_t	accum_flywheel_ticks;										// Currently 2 ticks per flywheel rev.
-} speed_event_t;
-#endif
-//
-//
-//
+	const uint8_t pin;
+	blink_pattern_e pattern;
+} pattern_t;
+#define PATTERN_LEN				4								// Working with 4 pins (2 LEDs, 2 pins each)
+#define LED_FRONT_GREEN_PIN			14
+#define LED_FRONT_RED_PIN			15
+#define LED_BACK_GREEN_PIN			16
+#define LED_BACK_RED_PIN			17
 
-#define RESISTANCE_SET_ERG 1
-#define RESISTANCE_SET_SIM 2
+#define PATTERN_DEFAULT			{			\
+									[LED_FRONT_GREEN] = { .pin = LED_FRONT_GREEN_PIN, 	.pattern = 0b00000000 }, 	\
+									[LED_FRONT_RED] =	{ .pin = LED_FRONT_RED_PIN, 	.pattern = 0b00000000 },	\
+									[LED_BACK_GREEN] =	{ .pin = LED_BACK_GREEN_PIN, 	.pattern = 0b00000000 },	\
+									[LED_BACK_RED] =	{ .pin = LED_BACK_RED_PIN, 		.pattern = 0b00000000 }		\
+								}
 
-speed_event_t buffer[8];
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c\r\n"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
 
-void print_speed(speed_event_t* p_speed) 
+// active pattern per pin
+static pattern_t active_pattern[PATTERN_LEN] = PATTERN_DEFAULT;
+// storage for patterns that repeat
+static blink_pattern_e repeating_pattern[PATTERN_LEN];
+
+static void pattern_set(led_e led, blink_pattern_e pattern, bool repeated)
 {
-	printf("Hello World, %i, %i\r\n", 
-		p_speed->event_time_2048, 
-		p_speed->accum_flywheel_ticks);
-}
+	uint8_t led_pair = (led & 0x1) ? led-1: led+1;		// LEDs are in pairs, get it's pair
+	active_pattern[led].pattern = pattern;				// Set the active patter for the specified led
+	active_pattern[led_pair].pattern ^= (pattern & active_pattern[led_pair].pattern); 		// XOR of matching bits.
 
-bool test(bool m_resistanceChangeQueued, uint16_t event_count, uint8_t resistance_mode)
-{
-	if ( m_resistanceChangeQueued == true || 
-		(event_count % 8 == 0 && (resistance_mode == RESISTANCE_SET_ERG ||
-			resistance_mode == RESISTANCE_SET_SIM)) )
+	/*LED_LOG("[LED] setting: %i, pair: %i, pattern: %i, pair: %i\r\n",
+			led, led_pair, active_pattern[led].pattern, active_pattern[led_pair].pattern);*/
+
+	if (repeated)
 	{
-		return true;
+		repeating_pattern[led] = active_pattern[led].pattern;
+		repeating_pattern[led_pair] = active_pattern[led_pair].pattern;
 	}
-	else
-	{
-		return false;
-	}
+
+	printf("LED(%i)      "BYTE_TO_BINARY_PATTERN, led, BYTE_TO_BINARY(active_pattern[led].pattern));
+	printf("LED_PAIR(%i) "BYTE_TO_BINARY_PATTERN, led_pair, BYTE_TO_BINARY(active_pattern[led_pair].pattern));
+
+	/*
+	printf("active == %i\r\npair == %i\r\n", 
+		active_pattern[led].pattern,
+		active_pattern[led_pair].pattern);*/
+
 }
 
 int main(int argc, char *argv [])
-{	
-	bool result = test(false, 5, RESISTANCE_SET_ERG);
-	printf("result: %i\r\n", result); 
-	return;
+{
+	pattern_set(LED_FRONT_GREEN, FAST_BLINK_2, false);
 
-	printf("Any key to start.");
-	getchar();
+	pattern_set(LED_FRONT_GREEN, SOLID, true);
+	// 168 == 1010 1000
+	// 87  == 0101 0111 
 
-	// init
-	event_fifo_t fifo = speed_event_fifo_init((uint8_t*)buffer, 8);
-	speed_event_t* p_event;
-
-	for (int i = 0; i< 47; i++) {
-		speed_event_t speed1 = { .event_time_2048 = 1 * i, .accum_flywheel_ticks = 100 * i };
-		p_event = (speed_event_t*)speed_event_fifo_put(&fifo, (uint8_t*)&speed1);
-		print_speed(p_event);
-	}
-
-	speed_event_t* p_speed = (speed_event_t*)speed_event_fifo_oldest(&fifo);
-	print_speed(p_speed); // oldest in the fifo.
-
-	p_speed = (speed_event_t*)speed_event_fifo_get(&fifo);
-	print_speed(p_speed); // most recent one.
-
-	return 0;
+	pattern_set(LED_FRONT_RED, FAST_BLINK_3, false);
 }
