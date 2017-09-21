9f9fed78e81787789db1a389145e12984defa896
Jason De Lorme <jjdelorme@yahoo.com>
2017-09-21 17:25:13 -0400

    Using the new file format for DFU apps to work.

--- python/deploy/deploy.py
+++ python/deploy/deploy.py
@@ -2,26 +2,28 @@ import sys
 import zipfile as zip
 import os
 
-def deploy(hex_path):
+def deploy(bin_path):
 	# archive name is the same, except ends in .zip
-	zip_path = hex_path.replace('.hex', '.zip')
+	zip_path = bin_path.replace('.bin', '.zip')
 	appdat_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'application.dat')
+	manifest_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'manifest.json')
 	
 	with zip.ZipFile(zip_path, 'w') as zipPkg:
-		zipPkg.write(hex_path, 'application.hex')
+		zipPkg.write(bin_path, 'application.bin')
 		zipPkg.write(appdat_path, os.path.basename(appdat_path))
+		zipPkg.write(manifest_path, os.path.basename(manifest_path))
 		
 	return zip_path
 
 		
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
-		print("ERROR! No path to hex specified\nusage: deploy.py <path_to_hex>")
+		print("ERROR! No path to bin specified\nusage: deploy.py <path_to_bin>")
 		sys.exit(2)
-	hex_path = sys.argv[1]
+	bin_path = sys.argv[1]
 
 	try:
-		zip_path = deploy(hex_path)
+		zip_path = deploy(bin_path)
 		print("SUCCESS! Created archive at %s." % zip_path)
 	except:
 		print("ERROR unable to create zip archive.")		
